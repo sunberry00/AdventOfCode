3a06d4f545bbd16734db83a6f419d19ab68180fc
Alexander <alexander.ivanets@rwth-aachen.de>
2023-12-01 11:11:14 +0100

    Add Day1, Part1

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -1,20 +1,26 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> lines = Files.readAllLines(Path.of("input"));
+        List<String> lines = Files.readAllLines(Path.of("./Day1/src/input"));
 
+        //Part 1
         long sum = lines.stream()
                 .mapToInt(line -> {
-                    String[] digits = line.split("\\D");
+                    String[] digits = Arrays.stream(line.split(""))
+                            .filter(d -> d.matches("\\d"))
+                            .toArray(String[]::new);
                     return digits.length != 1
-                            ? Integer.parseInt(digits[0]) + Integer.parseInt(digits[digits.length - 1])
-                            : Integer.parseInt(digits[0]) * 2;
+                            ? Integer.parseInt(digits[0] + digits[digits.length - 1])
+                            : Integer.parseInt(digits[0] + digits[0]);
                 }).sum();
 
-        System.out.println(sum);
+
+
     }
 }
