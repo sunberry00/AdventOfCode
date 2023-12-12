12384776b1c9a90c8baeb41eccf548d6026a10b7
Alexander <alexander.ivanets@rwth-aachen.de>
2023-12-12 15:43:12 +0100

    Add Day12 Approach for Part2

--- 2023/Day12/src/HotSprings.java
+++ 2023/Day12/src/HotSprings.java
@@ -1,46 +1,75 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.IntStream;
 
 public class HotSprings {
 
-    public static void firstPart(List<String> lines) {
-        List<Springs> inputSprings = lines
-                .stream()
-                .map(item -> new Springs(item.split(" ")[0], item.split(" ")[1].split(",")))
-                .toList();
+    public record Spring(String map, List<Integer> groups) {}
 
-        Function<String[], String[]> allPossibleArrangements = arr ->
-                IntStream.range(0, arr.length)
-                .mapToObj(i -> i < arr.length - 1 ?
-                        new String[]{arr[i] + "#", arr[i] + "."} :
-                        new String[]{arr[i]})
-                .reduce((arr1, arr2) -> Arrays.stream(arr1)
-                        .flatMap(str1 -> Arrays.stream(arr2)
-                                .map(str2 -> str1 + str2))
-                        .toArray(String[]::new))
-                .orElse(null);
+    public static List<Integer> fromStringToGroups(String[] strAmounts) {
+        return Arrays.stream(strAmounts).map(Integer::valueOf).toList();
+    }
 
-        for (Springs spr : inputSprings) {
-            int numberOfQuestions = (int) Arrays.stream(spr.getRepresentation().split("")).filter(ch -> ch.equals("?")).count();
-            String[] splitedStr = spr.getRepresentation().split("\\?", (int) Math.pow(2, numberOfQuestions));
-            String[] res = allPossibleArrangements.apply(splitedStr);
-            spr.setArrangements(res);
+    public static List<Integer> fromStringToGroupsPartTwo(String[] strAmounts) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 5; ++i) {
+            result.addAll(Arrays.stream(strAmounts).map(Integer::valueOf).toList());
+        }
+        return result;
+    }
+    public static boolean isRightArrangement(String arrangement, List<Integer> groups) {
+        String[] s = Arrays.stream(arrangement.split("\\.+")).filter(x -> !x.isEmpty()).toArray(String[]::new);;
+        if (s.length == groups.size()) {
+            for (int i = 0; i < s.length; ++i) {
+                if (s[i].length() != groups.get(i)) {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public static long numberOfArrangements(Spring spring) {
+        return numberOfArrangements(spring, new HashMap<>());
+    }
+
+    public static long numberOfArrangements(Spring spring, HashMap<Spring, Long> memo) {
+        if (spring.map().indexOf('?') == -1) {
+            if (isRightArrangement(spring.map(), spring.groups())) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
 
+        if (memo.containsKey(spring)) {
+            return memo.get(spring);
+        }
 
-        System.out.println(
-                inputSprings.parallelStream()
-                .mapToInt(Springs::countArrangements)
-                .sum());
-
+        long y = numberOfArrangements(new Spring(spring.map().replaceFirst("\\?", "."), spring.groups()), memo)
+                + numberOfArrangements(new Spring(spring.map().replaceFirst("\\?", "#"), spring.groups()), memo);
+        memo.put(spring, y);
+        return y;
     }
+
     public static void main(String[] args) throws IOException {
-        List<String> lines = Files.readAllLines(Path.of("./2023/Day12/input"));
-        firstPart(lines);
+        List<String> lines = Files.readAllLines(Path.of("./2023/Day12/example"));
+
+        List<Spring> hotSprings = lines.parallelStream()
+                .map(line -> new Spring(
+                        (line.split(" ")[0]) + ("?" + line.split(" ")[0]).repeat(4),
+                        fromStringToGroupsPartTwo(line.split(" ")[1].split(","))))
+                .toList();
+
+        System.out.println(hotSprings
+                .parallelStream()
+                .mapToLong(HotSprings::numberOfArrangements)
+                .sum());
     }
 }
